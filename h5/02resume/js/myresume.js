121635501f1bf1d49fd2a1f9750a1f7b5adc4a59
Sandra <sandradan411@163.com>
2019-08-13 18:18:46 +0800

    resume

---
+++ h5/02resume/js/myresume.js
@@ -0,0 +1,7 @@
+var change = document.getElementById('change');
+var paint = document.getElementById('paint');
+paint.onclick = function(){
+        change.style.color="blue";
+        change.style.fontSize = '18px';
+        change.style.border = 'dashed 1px #999999';
+}
